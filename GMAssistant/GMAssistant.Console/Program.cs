60da01795161a7438a832fb0680d2637fba5ffa1
Paul Cloys <paul.cloys@gmail.com>
2018-12-29 16:20:43 -0500

    Added a .NET Core console project.  Core may not be the direction we want to go, but I haven't dug into what kinds of UI we may want and how to deploy something to Pi

---
+++ GMAssistant/GMAssistant.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GMAssistant.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.Read();
+        }
+    }
+}
